8602919a2e391fb2591b95c144790589949bedc6
Lamia Fatiha <f6lamia@gmail.com>
2024-07-06 22:08:00 +0600

    image imported

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import tv from '../assets/image/tv.png'
 
 const Nav = () => {
   return (
     <div>
       hello there
+      <img src={tv} alt="" />
     </div>
   )
 }
