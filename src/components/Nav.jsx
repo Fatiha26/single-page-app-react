cf296cac3e872531d3177b92da704e755eb1eba4
Lamia Fatiha <f6lamia@gmail.com>
2024-07-11 20:43:56 +0600

    scroll animation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,11 +6,11 @@ const Nav = () => {
   <>
     <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
       <ul className="text-center text-xl p-20">
-        <Link to="Home"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li></Link>
-        <Link to="About"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li></Link>
-        <Link to="Services"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Services</li></Link>
-        <Link to="Projects"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Projects</li></Link>
-        <Link to="Contact"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li></Link>
+        <Link spy={true} smooth={true} to="Home"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li></Link>
+        <Link spy={true} smooth={true} to="About"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li></Link>
+        <Link spy={true} smooth={true} to="Services"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Services</li></Link>
+        <Link spy={true} smooth={true} to="Projects"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Projects</li></Link>
+        <Link spy={true} smooth={true} to="Contact"><li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li></Link>
       </ul>
     </div>
   </>
